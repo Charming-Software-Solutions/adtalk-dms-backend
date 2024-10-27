cce4cfe73e4f62b532bbd80710bf0cb91d9beb18
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-10-27 15:50:02 +0800

    refactor(asset): added type representation

--- asset/serializers.py
+++ asset/serializers.py
@@ -13,3 +13,8 @@ class AssetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Asset
         fields = "__all__"
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response["type"] = AssetTypeSerialiezr(instance.type).data
+        return response
